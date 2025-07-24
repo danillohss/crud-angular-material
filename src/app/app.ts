9ef9efafbd37514f8d84df656bd454f471face1d
Danillo Soares <danillohss@gmail.com>
2025-07-24 16:20:47 -0300

    Angular routerLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatToolbar],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatToolbar,
+    RouterLink,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
