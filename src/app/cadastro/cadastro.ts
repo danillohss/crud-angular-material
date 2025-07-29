1782756082e7f659e7db6c8f67e46479e8170e46
Danillo Soares <danillohss@gmail.com>
2025-07-29 17:00:47 -0300

    roteamento

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { Cliente } from './cliente';
 import { clienteService } from '../clienteService';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   imports: [
     FlexLayoutModule,
@@ -21,10 +22,26 @@ import { clienteService } from '../clienteService';
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {
+export class Cadastro implements OnInit {
   cliente: Cliente = Cliente.newCliente();
-  constructor(private service: clienteService) {}
+  atualizando: boolean = false;
+  constructor(private service: clienteService, private route: ActivatedRoute) {}
   salvarCliente() {
     this.service.salvar(this.cliente);
+    this.cliente = Cliente.newCliente();
+  }
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((query: any) => {
+      const params = query['params'];
+      const id = params['id'];
+      if (id) {
+        let clienteEncontrado = this.service.buscarClientePorId(id);
+        if (clienteEncontrado) {
+          this.atualizando = true;
+          this.cliente =
+            this.service.buscarClientePorId(id) || Cliente.newCliente();
+        }
+      }
+    });
   }
 }
